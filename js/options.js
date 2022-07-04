c7b8761d85fa210381bf96e144f5f156570d9fc0
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2022-07-04 06:47:10 -0600

    Save settings in ls

--- js/options.js
+++ js/options.js
@@ -1,3 +1,4 @@
+import { readFromLS, writeToLS } from './utilities/localStorage.js';
 export default class Options {
   constructor() {}
 
@@ -9,11 +10,41 @@ export default class Options {
       document.getElementById('options-menu').style.display = 'flex';
       return;
     }
-    console.log('closing menu');
     this.closeMenu();
   }
 
   closeMenu() {
     document.getElementById('options-menu').style.display = 'none';
+    this.saveSettings();
+  }
+
+  saveSettings() {
+    // Get the different radio buttons
+    const veryEasy = document.getElementById('very-easy');
+    const easy = document.getElementById('easy');
+    const medium = document.getElementById('medium');
+    const hard = document.getElementById('hard');
+    const extreme = document.getElementById('extreme');
+
+    const difficultySettings = [veryEasy, easy, medium, hard, extreme];
+
+    const slow = document.getElementById('slow');
+    const normal = document.getElementById('normal');
+    const fast = document.getElementById('fast');
+
+    const speedSettings = [slow, normal, fast];
+
+    // Set the settings in Local Storage
+    difficultySettings.forEach((radioButton) => {
+      if (radioButton.checked) {
+        writeToLS('difficulty', radioButton.value);
+      }
+    });
+
+    speedSettings.forEach((radioButton) => {
+      if (radioButton.checked) {
+        writeToLS('zombie-speed', radioButton.value);
+      }
+    });
   }
 }
